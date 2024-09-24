c92d640c96765af764bba1d8076ce9df39d5a5fb
mahdi786islam <mahdiislam786@gmail.com>
2024-09-24 23:32:41 +0600

    toggle history

--- js/app.js
+++ js/app.js
@@ -57,3 +57,10 @@ document.getElementById('quota-donate-btn').addEventListener('click',function(){
     
     }
 )
+
+document.getElementById('donation-btn').addEventListener('click', function(){
+    showSectionById('donation-section')
+})
+document.getElementById('history-btn').addEventListener('click', function(){
+    showSectionById('history-section')
+})
